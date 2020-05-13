9a1250db0e75c6f7d4ce003d1b5ce346ffcfbb1a
sisbet <48634874+sisbet@users.noreply.github.com>
2020-05-13 04:47:39 +0200

    jump, animation

--- UpperLevel/Assets/Scripts/playerMotor.cs
+++ UpperLevel/Assets/Scripts/playerMotor.cs
@@ -1,25 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
 
 public class playerMotor : MonoBehaviour
 {
     private CharacterController controller;
 
-    private float verticalVelocity;
-    private float gravity = 7.0f;
-    private float jumpForce = 3.5f;
+    //private float verticalVelocity;
+    //private float gravity = 7.0f;
+    
+    public float speed = 7f;
+    private Rigidbody rb;
+    public LayerMask groundLayers;
+    public float jumpForce = 7f;
+    private SphereCollider col;
 
-  
+    static Animator animator;
+      
 
     void Start()
     {
-        controller = GetComponent<CharacterController>();
+        //controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<SphereCollider>();
     }
 
     void Update()
     {
-      if (controller.isGrounded)
+        float horizontal = Input.GetAxis("Horizontal");
+        animator.SetFloat("horizontal", horizontal);
+        float translation = horizontal * speed * Time.deltaTime;
+        transform.Translate(Vector3.forward * Mathf.Abs(translation));
+
+        // da considerare per miglioramenti al movimento
+        //rb.AddForce(Vector3.forward * horizontal * speed);
+
+        if (translation != 0)
+        {
+            animator.SetBool("isMoving", true);
+            if (translation < 0)
+            {
+                transform.rotation = Quaternion.Euler(0f, 90f, 0f);
+            }
+            else
+            {
+                transform.rotation = Quaternion.Euler(0f, -90f, 0f);
+            }
+
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            animator.SetTrigger("isJumping");
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }              
+         
+        //parte vecchia con il character controller 
+        /*if (controller.isGrounded)
         {
             verticalVelocity = -gravity * Time.deltaTime;
             
@@ -36,8 +79,13 @@ public class playerMotor : MonoBehaviour
 
         Vector3 moveVector = Vector3.zero;
         moveVector.y = verticalVelocity;
-        moveVector.x = Input.GetAxis("Horizontal") * 8f;
+        //moveVector.x = horizontal * speed;
 
-        controller.Move(moveVector*Time.deltaTime);
+        controller.Move(moveVector*Time.deltaTime);*/
     }
+
+    private bool IsGrounded()
+    {
+        return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x, col.bounds.min.y, col.bounds.center.z), col.radius*9f, groundLayers);
+            }
 }

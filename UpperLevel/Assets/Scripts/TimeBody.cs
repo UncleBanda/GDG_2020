aae97dbb886b7cf57a13c89a3a917663322ad0ed
marco797 <65222383+marco797@users.noreply.github.com>
2020-05-13 14:56:42 +0200

    Implementazione Tempo
    Implementata abilità del tempo con relativa barra

---
+++ UpperLevel/Assets/Scripts/TimeBody.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeBody : MonoBehaviour
+{
+    public float TimeBeforeAffected; //The time after the object spawns until it will be affected by the timestop(for projectiles etc)
+    private TimeManager timemanager;
+    private Rigidbody rb;
+    private Vector3 recordedVelocity;
+    private float recordedMagnitude;
+    private Animator anim;
+
+    private float TimeBeforeAffectedTimer;
+    private bool CanBeAffected;
+    private bool IsStopped;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        timemanager = GameObject.FindGameObjectWithTag("TimeManager").GetComponent<TimeManager>();
+        TimeBeforeAffectedTimer = TimeBeforeAffected;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeBeforeAffectedTimer -= Time.deltaTime; // minus 1 per second
+        if (TimeBeforeAffectedTimer <= 0f)
+        {
+            CanBeAffected = true; // Will be affected by timestop
+        }
+
+        if (CanBeAffected && timemanager.TimeIsStopped && !IsStopped)
+        {
+            if (rb.velocity.magnitude >= 0f) //If Object is moving
+            {
+                recordedVelocity = rb.velocity.normalized; //records direction of movement
+                recordedMagnitude = rb.velocity.magnitude; // records magitude of movement
+
+                rb.velocity = Vector3.zero; //makes the rigidbody stop moving
+                rb.isKinematic = true; //not affected by forces
+                IsStopped = true; // prevents this from looping
+            }
+        }
+        if (timemanager.TimeIsStopped)
+        {
+            
+            anim.speed = 0f;  //stop the animation
+        }
+        else
+        {
+            anim.speed = 1f;
+        }
+
+    }
+    public void ContinueTime()
+    {
+        rb.isKinematic = false;
+        IsStopped = false;
+        rb.velocity = recordedVelocity * recordedMagnitude; //Adds back the recorded velocity when time continues
+    }
+}

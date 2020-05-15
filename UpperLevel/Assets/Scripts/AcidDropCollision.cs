0c43f86b87d48a000c1c6b7c97042b48b436d7aa
marco797 <65222383+marco797@users.noreply.github.com>
2020-05-15 13:19:33 +0200

    Riparato salto e messo acido
    Aggiunto primo ostacolo cido che cade dal tetto

---
+++ UpperLevel/Assets/Scripts/AcidDropCollision.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AcidDropCollision : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject drop;
+    private Rigidbody rb;
+    private Vector3 originalPos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = drop.GetComponent<Rigidbody>();
+        originalPos = drop.transform.localPosition;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == drop.name)
+        {
+            rb.velocity = Vector3.zero;
+            drop.transform.localPosition = originalPos;
+
+        }
+    }
+
+
+}

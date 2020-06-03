c0b11f935d0123c434b64dda4c3216dcdce9b082
sisbet <48634874+sisbet@users.noreply.github.com>
2020-06-03 18:30:13 +0200

    potionn+gamestatus added

---
+++ UpperLevel/Assets/Scripts/Potionn.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+
+
+public class Potionn : MonoBehaviour
+{
+    int ph;
+    GameObject principessa;
+    public Quaternion currentAngle;
+    public bool isEntered = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ph = GameStatus.GetPhase();
+        currentAngle = Quaternion.Euler(0,0, ph * 45);
+        principessa = GameObject.Find("principessinarig1");
+        Debug.Log("" + principessa.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, currentAngle, 0.1f);
+        if (Input.GetKeyDown(KeyCode.I) && isEntered)
+        {
+            ph += 1;
+            if (ph > 1)
+            {
+                ph = -1;
+            }
+            currentAngle = Quaternion.Euler(0, 0, ph * 45);
+            
+           
+            GameStatus.SetPhase(ph);
+           
+        }
+               
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+       
+        if (other.name == principessa.name)
+        {
+            isEntered = true;
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.name == principessa.name)
+        {
+            isEntered = false;
+        }
+    }
+
+}
---
+++ UpperLevel/Assets/Scripts/acidPhase.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class acidPhase : MonoBehaviour
+{
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        int phase = GameStatus.GetPhase();
+        if (phase != 0)
+        {
+            transform.Translate(Vector3.up * phase* 7f);
+            
+        }
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

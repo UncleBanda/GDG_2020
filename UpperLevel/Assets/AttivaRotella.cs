545ce74080d42edb228ee50015627a2d3dc8ca62
marco797 <65222383+marco797@users.noreply.github.com>
2020-07-16 11:29:28 +0200

    ModificheBanda2

---
+++ UpperLevel/Assets/AttivaRotella.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttivaRotella : MonoBehaviour
+{
+    public GameObject player;
+    //public GameObject levaUp;
+    //public GameObject levaMiddle;
+    //public GameObject levaDown;
+
+    public Rigidbody rotella;
+
+    bool isInside = false;
+
+    void Update()
+    {
+        if (isInside == true)
+        {
+            if (Input.GetKey(KeyCode.I))
+            {
+                //levaUp.GetComponent<Animator>().enabled = true;
+                //levaMiddle.GetComponent<Animator>().enabled = true;
+                //levaDown.GetComponent<Animator>().enabled = true;
+
+                rotella.constraints &= ~RigidbodyConstraints.FreezePositionY;
+            }
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == player.name)
+        {
+            isInside = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == player.name)
+        {
+            isInside = false;
+        }
+    }
+}

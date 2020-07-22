37cedb1d3119f254baac229b3e17346a35e470d5
marco797 <65222383+marco797@users.noreply.github.com>
2020-07-22 18:00:02 +0200

    SliderTortaTime

---
+++ UpperLevel/Assets/OrologioSlider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OrologioSlider : MonoBehaviour
+{
+
+    public Slider slider;
+    public Transform LoadingBar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadingBar.GetComponent<Image>().fillAmount = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LoadingBar.GetComponent<Image>().fillAmount = slider.value/slider.maxValue;
+    }
+}

7f714d284a2a2af661345a934044893d9d65b31a
sisbet <48634874+sisbet@users.noreply.github.com>
2020-07-23 19:47:28 +0200

    wip menu

--- UpperLevel/Assets/Scripts/menu.cs
+++ UpperLevel/Assets/Scripts/menu.cs
@@ -1,9 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Runtime;
+using System.Runtime.Hosting;
 
 public class menu : MonoBehaviour
 {
+    public GameObject cameraMenu;
+    public GameObject menuCan;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +19,15 @@ public class menu : MonoBehaviour
     void Update()
     {
         
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("CaveLevel", LoadSceneMode.Single);
+        cameraMenu.SetActive(false);
+        menuCan.SetActive(false);
+    }
+    public void QuitGame()
+    {
+        UnityEngine.Application.Quit();
     }
 }
---
+++ UpperLevel/Assets/pausa.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pausa : MonoBehaviour
+{
+    public bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject options;
+    public GameObject menuIniziale;
+    public GameObject player;
+    public Animator animatorPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("principessinarig1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+
+            if (menuIniziale.activeSelf == false)
+            {
+                if (GameIsPaused)
+                {
+                    Resume();
+                }
+                else
+                {
+                    Pause();
+                }
+            }
+        }
+
+        if (player == null)
+        {
+            player = GameObject.FindWithTag("Player");
+            if (player != null)
+            {
+                animatorPlayer = player.transform.GetComponent<Animator>();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        animatorPlayer.enabled = true;
+    }
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        animatorPlayer.enabled = false;
+        options.SetActive(false);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Opzioni()
+    {
+        options.SetActive(true);
+        pauseMenuUI.SetActive(false);
+    }
+}

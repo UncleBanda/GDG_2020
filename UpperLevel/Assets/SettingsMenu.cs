b3993ec9574dc4a933bb16420fde01030d03e64e
marco797 <65222383+marco797@users.noreply.github.com>
2020-07-24 12:20:10 +0200

    MenuInserito

---
+++ UpperLevel/Assets/SettingsMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public TMP_Dropdown resolutionDropdown;
+    public AudioMixer audioMixer;
+    Resolution[] resolutions;
+   
+    void Start()
+    {
+        resolutionDropdown.ClearOptions();
+        resolutions = Screen.resolutions;
+        List<string> options = new List<string>();
+        int CurrentResolutionIndex = 0;
+        for ( int i=0; i<resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+
+            if (resolutions[i].width == Screen.width && resolutions[i].height == Screen.height)
+            {
+                CurrentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = CurrentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+        
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+    
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", Mathf.Log10(volume) * 20); 
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullScreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }    
+}
---
+++ UpperLevel/Assets/levelChange.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class levelChange : MonoBehaviour
+{
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+        public void FadeToLevel(int levelIndex)
+    {
+        animator.SetTrigger("fadeOut");
+    }
+}

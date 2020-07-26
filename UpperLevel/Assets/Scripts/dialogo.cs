9a07402060c5b354ddeece70ddeaee1296c80efb
sisbet <48634874+sisbet@users.noreply.github.com>
2020-07-26 23:49:18 +0200

    nanetti + suggerimenti

---
+++ UpperLevel/Assets/Scripts/dialogo.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialogo : MonoBehaviour
+{
+    public GameObject sugg;
+    private GameObject principessa;
+    public bool isEntered;
+    // Start is called before the first frame update
+    void Start()
+    {
+        principessa = GameObject.Find("principessinarig1");
+        sugg.active = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I) && isEntered && sugg.active == false)
+        {
+            sugg.active = true;
+            UnityEngine.Debug.Log("sdfdsfs");
+
+        }
+        else if (Input.GetKeyDown(KeyCode.I) && sugg.active == true)
+        {
+            sugg.active = false;
+        }
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.name == principessa.name)
+        {
+            isEntered = true;
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.name == principessa.name)
+        {
+            isEntered = false;
+        }
+    }
+}
